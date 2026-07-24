ca02b46f3bd685f0aa21e13ca5bdf569d92af6ed
Mohamad Supangat <moha.supangat@gmail.com>
2026-07-25 01:53:13 +0700

    feat: menambahkan c-j dan c-k

--- plugin/20_completion.lua
+++ plugin/20_completion.lua
@@ -16,5 +16,7 @@ local map_multistep = require('mini.keymap').map_multistep
 
 map_multistep('i', '<Tab>', { 'pmenu_next' })
 map_multistep('i', '<S-Tab>', { 'pmenu_prev' })
+map_multistep('i', '<C-j>', { 'pmenu_next' })
+map_multistep('i', '<S-k>', { 'pmenu_prev' })
 map_multistep('i', '<CR>', { 'pmenu_accept', 'minipairs_cr' })
 map_multistep('i', '<BS>', { 'minipairs_bs' })
